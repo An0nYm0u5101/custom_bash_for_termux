1f92e1c5e9fd36ed246a4ec5d9c11a5c4ac302c3
Samiul Joy <51329626+samiuljoy@users.noreply.github.com>
2020-01-24 19:26:51 +0600

    Update hep.sh

--- hep.sh
+++ hep.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+#Type in hep and then a command
+#eg. hep files
+#what it does is appends your commands with the --help 
+#subcommand. Instead of typing in for eg. files --help 
+#just type in hep files
+#Thought this might speed things up
 $1 --help
