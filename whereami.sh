bfb25a49bff2064e1b17716733047515decafe00
Samiul Joy <51329626+samiuljoy@users.noreply.github.com>
2020-01-24 19:38:38 +0600

    Update whereami.sh

--- whereami.sh
+++ whereami.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+#A simple Curl command to show your location
+#Usage whereami "any ip" to lookup your desired ip address
+#leave a space blank after whereami to see your own location
 echo "The IP address $1 is at:"
 curl https://freegeoip.app/xml/$1
