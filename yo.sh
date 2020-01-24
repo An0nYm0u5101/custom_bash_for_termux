f2b7fb63a55ac10885dcbe393e2bd3720602a96a
Samiul Joy <51329626+samiuljoy@users.noreply.github.com>
2020-01-24 19:42:46 +0600

    Update yo.sh

--- yo.sh
+++ yo.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+#type in yo "pkg" name to install a package along with update and upgrade 
+#options instead of typing in pkg install "pkg name"
 pkg update && pkg upgrade; pkg install $1
